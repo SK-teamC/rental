9d978fb3d71131ffda955f294f4edcf9f18df982
neosangsang <SKCC@SKCC16D00068.SKCC.NET>
2020-07-13 18:02:42 +0900

    first commit

---
+++ Delivery/src/main/java/rental/PolicyHandler.java
@@ -0,0 +1,33 @@
+package rental;
+
+import rental.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverOrdered_Delivery(@Payload Ordered ordered){
+
+        if(ordered.isMe()){
+            System.out.println("##### listener Delivery : " + ordered.toJson());
+        }
+    }
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverOrderCanceled_DeliveryCancel(@Payload OrderCanceled orderCanceled){
+
+        if(orderCanceled.isMe()){
+            System.out.println("##### listener DeliveryCancel : " + orderCanceled.toJson());
+        }
+    }
+
+}

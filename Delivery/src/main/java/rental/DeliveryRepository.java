eb373a7b055b529a29353c79ba2ad2ecad0c23fd
jasonchoi11 <hsloveme1@naver.com>
2020-07-14 11:07:56 +0900

    add findByOrderId Method in DeliveryRepository

--- Delivery/src/main/java/rental/DeliveryRepository.java
+++ Delivery/src/main/java/rental/DeliveryRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
 
-
+    Delivery findByOrderId(Long orderId);
 }
